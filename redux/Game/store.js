f030a3aa27b5ec1a550e06e38c46c6286089d0e9
unknown <subhash227@yahoo.com>
2021-04-29 17:33:56 +1000

    logger console log stopped

--- redux/Game/store.js
+++ redux/Game/store.js
@@ -1,9 +1,16 @@
 import { createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension";
 import Reducer from "./reducer";
+const middlewares = [];
+
+if (process.env.NODE_ENV === `development`) {
+  const { logger } = require(`redux-logger`);
+
+  middlewares.push(logger);
+}
+
 const store = createStore(
   Reducer,
-  composeWithDevTools(applyMiddleware(logger))
+  composeWithDevTools(applyMiddleware(...middlewares))
 );
 export default store;
